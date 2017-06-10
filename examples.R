cf1d6384584b34abc64a3e40c4982d6639f53f90
Michał Błotniak <buoto@buzytor.com>
2017-06-10 18:54:23 +0200

    Add realistic example

--- examples.R
+++ examples.R
@@ -11,3 +11,16 @@ f <- polynomial(c(-1, -1, -2, -1, 0.2))
 plot(f, xlim=c(-10, 10))
 
 mea(list(c(-10, 10)), 10, as.function(f))
+
+source('simulation.R')
+simulate(rep(1/2, 24), rep(1, 4), rep(0, 4), rep(0, 4), rep(1, 4), list(theta = 5))
+
+a <- replicate(100, simulate(rep(1/2, 24), list(c(1, 0, 0, 1), c(1, 0, 0, 1), c(1, 0, 0, 1), c(1, 0, 0, 1)), list(theta = 5)))
+
+results <- c()
+for (i in 1:10) {
+  a <- replicate(100, simulate(rep(1/2, 720),
+                               list(morning = c(2, 2, 1, 0.5), noon = c(0.5, 0.5, 0.2, 0.1), afternoon = c(2, 2, 0.8, 0.8), night = c(0.3, 0.2, 0.05, 0.1)),
+                               list(theta = i)))
+  results <- c(results, list(mean = mean(a), sd = sd(a)))
+}
--- simulation.R
+++ simulation.R
@@ -2,49 +2,43 @@ library(stats)
 library(magrittr)
 library(fdrtool)
 
-roadVector <- function(N = 0, S = 0, W = 0 , E = 0) c(N = N, S = S,  W = W, E = E)
-
-simulate <- function(parameters, distsN, distsS, distsW, distsE, distEscape) {
+simulate <- function(parameters, trafficDists, distEscape) {
   if (parameters %>% length %>% mod(24) > 0) stop('parameters number must be dividable by 24')
   
   dayPhaseSteps <- length(parameters) / 24 * c(4, 5, 4, 11)
-  waitingCars <- roadVector()
+  waitingCars <- c(0, 0, 0, 0)
   waitTime <- 0
   step <- 0
   phaseEnd <- 0
-  currentMoves <- roadVector()
+  currentMoves <- c(0, 0, 0, 0)
   
   for (phase in 1:length(dayPhaseSteps)) {
     phaseEnd <- phaseEnd + dayPhaseSteps[[phase]]
-    print(c("phase", phase))
+    phaseDists <- trafficDists[[phase]]
     
     while (step < phaseEnd) {
       # Update waiting time with cars left
       waitTime <- waitTime + sum(waitingCars)
       
       # Simulate arriving cars
-      waitingCars <- waitingCars + c(rpois(1, distsN[[phase]]), rpois(1, distsS[[phase]]), rpois(1, distsW[[phase]]), rpois(1, distsE[[phase]]))
-      print(c("step", step, waitingCars))
+      waitingCars <- waitingCars + rpois(length(phaseDists), phaseDists)
       
       cycles <- list(c(1, 2), c(3, 4))
       cycleEnd <- floor(step) + parameters[floor(step) + 1]
+      
       for (cc in cycles) {
         while (step < cycleEnd && any(waitingCars[cc] > 0)) {
           # Update current moves
-          print(c("moves1", currentMoves))
-          
           startingDirections <- waitingCars[cc] > 0 & currentMoves[cc] <= step
           currentMoves[cc][startingDirections] <- step + rhalfnorm(sum(startingDirections), distEscape$theta)
           
-          waitTime <- (waitTime + (step %% 1)) * sum(startingDirections)
+          waitTime <- waitTime + (mod(step, 1) * sum(startingDirections))
           
           step <- min(currentMoves[currentMoves > step])
           direction <- currentMoves == step
           
           # Handle current car's move
-          print(c("moves1", currentMoves))
           waitingCars[direction] <- waitingCars[direction] - 1
-          print(c("left1", waitingCars))
         }
         
         if (step < cycleEnd) {
@@ -53,12 +47,8 @@ simulate <- function(parameters, distsN, distsS, distsW, distsE, distEscape) {
         } else if (any(currentMoves > step)) {
           # Let last car leave the crossroad
           direction <- currentMoves > step
-          print(c("moves2", currentMoves))
-          
           step <- currentMoves[direction]
-          
           waitingCars[direction] <- waitingCars[direction] - 1
-          print(c("left2", waitingCars))
         }
         
         cycleEnd <- floor(step + 1)
@@ -68,6 +58,3 @@ simulate <- function(parameters, distsN, distsS, distsW, distsE, distEscape) {
   
   waitTime
 }
-
-simulate(rep(1/2, 24), rep(1, 4), rep(0, 4), rep(0, 4), rep(1, 4), list(theta = 5))
-
